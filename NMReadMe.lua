0e12c3a2e0b5532ef0ad7ab05f0059eb4b6e56c0
NINE-Lua <Trallen10@yahoo.com>
2025-06-09 14:41:44 -0500

    Create NMReadMe.lua

---
+++ NMReadMe.lua
@@ -0,0 +1,11 @@
+Put loadstring before, then you can use like this;
+
+notificationTitle = "Update"
+notificationMessage = "New feature added!"
+showNotification(5)
+
+-------------------------------------------------------
+
+notificationTitle = the bigger text in the notification, the title.
+notificationMessage = the smaller text in the notification, the subtext some may say.
+showNotification(num) = num is the number of seconds the message shows for, if it doesn't get one, it'll goto 2.5 seconds.
